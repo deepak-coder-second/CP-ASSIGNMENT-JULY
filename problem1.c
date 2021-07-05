3823ffad8e63d99f3a96b0d7a9f2bbd6d50c8249
Deepak M S <souparnikad40@gmail.com>
2021-07-05 06:50:13 +0530

    problem 1 finished . Pending problem 2,5

---
+++ problem1.c
@@ -0,0 +1,40 @@
+//TODO: find the sum of diagonal elements of a matrix
+#include <stdio.h>
+
+//sum of diagonal number function declaration
+void sumOfDiagonal(int arr[50][50],int size);
+
+//main function
+int main(){
+    //declaration and intialization of variables
+    int matrix[50][50];
+    int matrixSize,i,j;
+    //get the row size or column size
+    printf("row size or column size:");
+    scanf("%d",&matrixSize);
+    //get the elements in matrix
+    printf("Enter the Elements of matrix");
+    for ( i = 0; i < matrixSize; i++)
+    {
+        for ( j = 0; j < matrixSize; j++)
+        {
+           scanf("%d",&matrix[i][j]);
+        }
+        
+    }
+    //function calling
+    sumOfDiagonal(matrix,matrixSize);
+    
+}
+
+void sumOfDiagonal(int arr[50][50],int size){
+    int i;
+    int sum = 0;
+    for ( i = 0; i < size; i++)
+    {
+        sum = sum + arr[i][i];
+    }
+
+    printf("\n sum = %d \n",sum);
+    
+}
